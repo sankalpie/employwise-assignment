e03dfa08021c130cdf1bf15ae0b1fcdd9c250618
Sankalp Pareek <sankalpie0903@gmail.com>
2024-12-14 18:25:59 +0530

    fixed the disabling and functionality of  next and prev button

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <ChakraProvider>
       <AuthProvider>
           <Routes>
+            <Route path="/" element={<Login/>}/>
             <Route path="/login" element={<Login />} />
             <Route
               path="/users"
--- src/pages/UsersList.js
+++ src/pages/UsersList.js
@@ -4,46 +4,61 @@ import axios from "axios";
 import { useAuth } from "../AuthContext";
 
 const UsersList = () => {
-  const { token } = useAuth();
-  const [users, setUsers] = useState([]);
-  const [page, setPage] = useState(1);
+    const { token } = useAuth();
+    const [users, setUsers] = useState([]);
+    const [page, setPage] = useState(1);
+    const [numberOfPages, setNumberOfPages] = useState(2);
 
-  const fetchUsers = async (page) => {
-    const response = await axios.get(`https://reqres.in/api/users?page=${page}`);
-    setUsers(response.data.data);
-  };
+    const fetchUsers = async (page) => {
+        const response = await axios.get(`https://reqres.in/api/users?page=${page}`);
+        setUsers(response.data.data);
+        setNumberOfPages(response.data.total_pages);
+    };
 
-  useEffect(() => {
-    fetchUsers(page);
-  }, [page]);
+    const backHandler = () => {
+        setPage((prev) => Math.max(prev - 1, 1));
+    };
 
-  return (
-    <Box p="6">
-      <SimpleGrid columns={3} spacing={4}>
-        {users.map((user) => (
-          <VStack key={user.id} p="4" boxShadow="md" borderRadius="md">
-            <Image src={user.avatar} alt={user.first_name} borderRadius="full" />
-            <Text fontWeight="bold">
-              {user.first_name} {user.last_name}
-            </Text>
-          </VStack>
-        ))}
-      </SimpleGrid>
-      <Box mt="4" textAlign="center">
-        <Button
-          colorScheme="blue"
-          mr="2"
-          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-          disabled={page === 1}
-        >
-          Previous
-        </Button>
-        <Button colorScheme="blue" onClick={() => setPage((prev) => prev + 1)}>
-          Next
-        </Button>
-      </Box>
-    </Box>
-  );
+    const nextHandler = () => {
+        if (page <= (numberOfPages - 1)) {
+            setPage((prev) => prev + 1);
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers(page);
+    }, [page]);
+
+    return (
+        <Box p="6">
+            <SimpleGrid columns={3} spacing={4}>
+                {users.map((user) => (
+                    <VStack key={user.id} p="4" boxShadow="md" borderRadius="md">
+                        <Image src={user.avatar} alt={user.first_name} borderRadius="full" />
+                        <Text fontWeight="bold">
+                            {user.first_name} {user.last_name}
+                        </Text>
+                    </VStack>
+                ))}
+            </SimpleGrid>
+            <Box mt="4" textAlign="center">
+                <Button
+                    colorScheme="blue"
+                    mr="2"
+                    onClick={backHandler}
+                    disabled={page === 1}
+                >
+                    Previous
+                </Button>
+                <Button 
+                    colorScheme="blue" 
+                    onClick={nextHandler}
+                    disabled={page === numberOfPages}>
+                    Next
+                </Button>
+            </Box>
+        </Box>
+    );
 };
 
 export default UsersList;
